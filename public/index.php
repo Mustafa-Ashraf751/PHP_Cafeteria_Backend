d012dd7e04567dc99e8143af0a77c31619440328
Mustafa Ashraf <mustafaashraf123@yahoo.com>
2025-04-10 00:32:35 +0200

    feat:add pagination to the user model

--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
 <?php
-// Set headers at the VERY TOP before any output
-// header("Access-Control-Allow-Origin: http://localhost:5173");
-// header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-// header("Access-Control-Allow-Headers: Content-Type, Authorization");
-// header("Access-Control-Allow-Credentials: true");
+
+//Set headers at the VERY TOP before any output
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
 // Immediately handle OPTIONS requests
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header("HTTP/1.1 200 OK");
-    exit();
+  header("HTTP/1.1 200 OK");
+  exit();
 }
 
 // Now load dependencies
